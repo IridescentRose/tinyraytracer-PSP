92c86c6052789ed7c450b7f8add971d21139e59c
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2020-04-11 23:07:44 -0400

    Working build?

--- tinyraytracer.cpp
+++ tinyraytracer.cpp
@@ -43,6 +43,15 @@ struct Sphere {
     }
 };
 
+#include <pspkernel.h>
+#include <pspdebug.h>
+#include <pspmath.h>
+
+PSP_MODULE_INFO("Stardust", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_VFPU | THREAD_ATTR_USER);
+PSP_HEAP_SIZE_KB(-1024);
+
+
 Vec3f reflect(const Vec3f &I, const Vec3f &N) {
     return I - N*2.f*(I*N);
 }
